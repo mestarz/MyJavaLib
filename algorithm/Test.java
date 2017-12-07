42193667b9226cec65812849ba9f909620a23eaf
qmd_xuanlv <32675828+mestarz@users.noreply.github.com>
2017-12-07 19:18:03 +0800

    Create Test.java

---
+++ algorithm/Test.java
@@ -0,0 +1,26 @@
+/**
+ * Class <em>Test</em> 含有测试时常实用的函数
+ * 
+ * @param starysky
+ */
+public class Test{
+
+  /**
+   * <em>say</em> 打印int数组的所有成员变量
+   * 
+   * @param 待打印的int数组
+   */
+  public static void say(int[] a){
+    for(int i = 0; i < a.length; i++){
+      System.out.print(a[i]);
+      System.out.print(" ");
+    }
+    System.out.println("");
+  }
+
+
+  public static void main(String[] args) {
+    
+    
+  }
+}
